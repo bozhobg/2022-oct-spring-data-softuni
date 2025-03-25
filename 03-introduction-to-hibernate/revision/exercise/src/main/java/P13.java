8187da3dadfbca1e0477952c32a88c7467a1d29e
Bozho Georgiev <bozhobg@gmail.com>
2025-03-25 12:11:14 +0200

    Last problem (#13) solution to Introduction to Hibernate involiving delete chain between 3 related tables.

--- 03-introduction-to-hibernate/revision/exercise/src/main/java/P13.java
+++ 03-introduction-to-hibernate/revision/exercise/src/main/java/P13.java
@@ -1,15 +1,19 @@
+import entities.Address;
+import entities.Employee;
 import entities.Town;
 import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class P13 {
 
-    private static final String OUTPUT_FORMAT_SINGLE_ADDRESS = "%d address in %s deleted";
+    private static final String OUTPUT_FORMAT_SINGLE_OR_NO_ADDRESS = "%d address in %s deleted:";
     private static final String OUTPUT_FORMAT = "%d address in %s deleted";
     private static final String TOWN_NOT_FOUND_FORMAT = "Town: %s not found";
 
     public static void main(String[] args) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("soft_uni");
         EntityManager em = emf.createEntityManager();
 
@@ -18,27 +22,53 @@ public class P13 {
         Town town;
         try {
             town = em.createQuery("""
-                                    FROM Town AS t
-                                    WHERE t.name = :input
-                                    """,
-                            Town.class)
+                            FROM Town AS t
+                            WHERE t.name = :input
+                            """, Town.class)
                     .setParameter("input", input)
                     .setMaxResults(1)
                     .getSingleResult();
-        } catch (NoResultException nre) {
 
+        } catch (NoResultException nre) {
+//            TODO: sout: 0 addresses in `town` deleted ?
             em.close();
-            throw new NoResultException(String.format(TOWN_NOT_FOUND_FORMAT, input));
+            System.out.println(String.format(OUTPUT_FORMAT_SINGLE_OR_NO_ADDRESS, 0, input));
+            return;
+//            throw new NoResultException(String.format(TOWN_NOT_FOUND_FORMAT, input));
         }
 
         EntityTransaction tr = em.getTransaction();
-//        TODO: unfinished
-        tr.begin();
 
+        tr.begin();
+//        chain of clearing data Employee.address -> Address -> Town due to table relations
+
+        List<Address> townAddresses = em.createQuery("""
+                        FROM Address AS a
+                        WHERE a.town = :existingTown
+                        """, Address.class)
+                .setParameter("existingTown", town)
+                .getResultList();
+
+        List<Employee> employeesWithTownAddress = em.createQuery("""
+                        FROM Employee AS e
+                        WHERE e.address IN (:townAddresses)
+                        """, Employee.class)
+                .setParameter("townAddresses", townAddresses)
+                .getResultList();
+
+        employeesWithTownAddress.forEach(e -> e.setAddress(null));
+        townAddresses.forEach(em::remove);
         em.remove(town);
-        em.flush();
 
         tr.commit();
+
+        int addressCount = townAddresses.size();
+
+        if (addressCount > 1) {
+            System.out.printf(OUTPUT_FORMAT, addressCount, input);
+        } else {
+            System.out.printf(OUTPUT_FORMAT_SINGLE_OR_NO_ADDRESS, addressCount, input);
+        }
 
         em.close();
     }
